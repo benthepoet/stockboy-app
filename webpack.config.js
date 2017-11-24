04bd33b2f37bbb43d531ab9f55cc6d50aaf3caca
Ben Hanna <benpaulhanna@gmail.com>
2017-11-24 15:32:46 +0000

    Added setup for webpack.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const { resolve } = require('path');
+
+const PUBLIC_PATH = resolve(__dirname, 'public');
+const SRC_PATH = resolve(__dirname, 'src');
+
+module.exports = {
+    entry: `${SRC_PATH}/index.js`,
+    output: {
+        filename: 'bundle.js',
+        path: PUBLIC_PATH
+    },
+    module: {
+        rules: [
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: {
+                    loader: 'elm-webpack-loader',
+                    options: {
+                        cwd: __dirname
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: PUBLIC_PATH,
+        disableHostCheck: true,
+        historyApiFallback: true
+    }
+};
